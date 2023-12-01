be1ef3a3d5f32a5947ca03e9d2c2554dab6fa664
Enzo Porto <enzosgporto@gmail.com>
2023-12-01 12:05:09 -0500

    fixed path issues with main input

--- 2023/Day 1/main.py
+++ 2023/Day 1/main.py
@@ -2,7 +2,7 @@
 
 sumOfDigits = 0
 
-with open('enzoInput.txt') as f:
+with open('2023/Day 1/mainInput.txt') as f:
     contents = f.read()
     lineArray = contents.split("\n") #splits the code per line
 
@@ -22,16 +22,11 @@ with open('enzoInput.txt') as f:
 
 for a in range(len(digitsArray)):
     firstDigit = digitsArray[a][0] #sets first digit to be index 0 of the current list
-    print("First digit is {}".format(firstDigit))
-
     secondDigit = digitsArray[a][len(digitsArray[a])-1] #sets last digit to be index list length - 1 of the current list
-    print("Second digit is {}".format(secondDigit))
 
     digit = firstDigit + secondDigit #concanates both strings to create the digit
-    print("The digit is {}".format(digit))
 
     sumOfDigits += int(digit) #transforms string into number and adds it to the sum    
-    print("Therefore the sum is {}".format(sumOfDigits))
 
     a += 1
 
